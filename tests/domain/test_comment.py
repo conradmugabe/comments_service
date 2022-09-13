ce5c7d4ff1888259b843287cf9fcf87860d092ea
conrad-evans <conradmugabe@gmail.com>
2022-09-13 12:43:15 +0300

    added tests for comparing instances of comment model

--- tests/domain/test_comment.py
+++ tests/domain/test_comment.py
@@ -32,3 +32,26 @@ def test_comment_to_dict():
     comment = Comment.from_dict(init_dict)
 
     assert comment.to_dict() == init_dict
+
+
+def test_comment_model_comparison_match():
+    id = uuid.uuid4()
+    init_dict = {"id": id, "commentOnId": "test_blog_id", "commentBy": "test_user_id"}
+
+    comment1 = Comment.from_dict(init_dict)
+    comment2 = Comment.from_dict(init_dict)
+
+    assert comment1 == comment2
+
+
+def test_comment_model_comparison_mismatch():
+    id = uuid.uuid4()
+    init_dict = {"id": id, "commentOnId": "test_blog_id", "commentBy": "test_user_id"}
+
+    id = uuid.uuid4()
+    init_dict_1 = {"id": id, "commentOnId": "test_blog_id_1", "commentBy": "test_user_id_1"}
+
+    comment1 = Comment.from_dict(init_dict)
+    comment2 = Comment.from_dict(init_dict_1)
+
+    assert comment1 != comment2
