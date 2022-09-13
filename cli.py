b0cb331e0bdf7f00771230a71189202944cb7a5e
conrad-evans <conradmugabe@gmail.com>
2022-09-13 15:51:35 +0300

    added a cli file to run the app in the command line

---
+++ cli.py
@@ -0,0 +1,34 @@
+from src.repository.in_memory_repository import InMemoryRepository
+from src.use_cases.comment_list import comment_list_use_case
+
+comments = [
+    {
+        "id": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+        "commentOnId": "f853578c-329a-4e65-81b8-566c5dffa35b",
+        "commentBy": "f853578c-fc0f-4e65-61bf-743c5dffa35d",
+        "body": "test comment 1",
+    },
+    {
+        "id": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+        "commentOnId": "fe2c3196-aedd-487a-a08f-e0bdc0ec6e9a",
+        "commentBy": "fe2d3195-3e11-487a-a0ad-e0bdc0ec8b2c",
+        "body": "test comment 1",
+    },
+    {
+        "id": "913694c6-435a-4366-ba0d-da5334a611b2",
+        "commentOnId": "913694c6-435a-4366-ba0d-da5334a611b2",
+        "commentBy": "913694c6-435a-4366-ba0d-da5334a611b2",
+        "body": "test comment 1",
+    },
+    {
+        "id": "eed76e77-55c1-41ce-985d-ca49bf6c0585",
+        "commentOnId": "eed76e77-55c1-41ce-985d-ca49bf6c0585",
+        "commentBy": "eed76e77-55c1-41ce-985d-ca49bf6c0585",
+        "body": "test comment 1",
+    },
+]
+
+repository = InMemoryRepository(comments)
+result = comment_list_use_case(repository)
+
+print([comment.to_dict() for comment in result])
