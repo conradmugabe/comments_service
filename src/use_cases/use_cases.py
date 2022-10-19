f562ae1c03d32f696fa566522d02dc535a2036a9
conrad mugabe <conradmugabe@gmail.com>
2022-10-20 00:36:52 +0300

    feat: added all CRUD use cases for comments

--- src/use_cases/use_cases.py
+++ src/use_cases/use_cases.py
@@ -1,9 +1,47 @@
+from src.entities.comment import Comment
 from src.repository.repository import Repository
+from src.dto.comment import (
+    CommentBy,
+    CommentId,
+    CreateComment,
+    DeleteComment,
+    GetComments,
+    UpdateComment,
+)
 
 
 class UseCases:
     def __init__(self, repository: Repository) -> None:
         self.repository = repository
 
-    def listComments(self):
-        self.repository.getComments()
+    def listComments(self, props: GetComments):
+        return self.repository.getComments(props)
+
+    def addComment(self, props: CreateComment):
+        return self.repository.createComment(props)
+
+    def updateComment(self, props: UpdateComment):
+        commentId, commentBy = props.get("id"), props.get("commentBy")
+        comment = self.__getCommentById(commentId)
+        self.__isCommentAuthor(comment, commentBy)
+        return self.repository.updateCommentById(props)
+
+    def deleteComment(self, props: DeleteComment):
+        commentId, commentBy = props.get("id"), props.get("commentBy")
+        comment = self.__getCommentById(commentId)
+        self.__isCommentAuthor(comment, commentBy)
+        return self.repository.deleteCommentById(commentId)
+
+    def getCommentById(self, id: CommentId):
+        return self.__getCommentById(id)
+
+    def __getCommentById(self, id: CommentId):
+        comment = self.repository.getCommentById(id)
+        if comment is None:
+            raise Exception("No comment with given Id found")
+        return comment
+
+    def __isCommentAuthor(self, comment: Comment, id: CommentBy):
+        if comment.commentBy == id:
+            return True
+        raise Exception("403! you don't have the permission to update this resource")
