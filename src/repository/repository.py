12013886ae32d5ab08e76ef2cb4f91fcda9e3f4f
conrad mugabe <conradmugabe@gmail.com>
2022-10-20 00:35:46 +0300

    fix: added more classes to abstract repository class

--- src/repository/repository.py
+++ src/repository/repository.py
@@ -1,14 +1,27 @@
 from abc import ABC, abstractclassmethod
-from typing import List
+from typing import List, Union
 
-from src.domain.comment import Comment
+from src.entities.comment import Comment
+from src.dto.comment import CommentId, CreateComment, GetComments, UpdateComment
 
 
 class Repository(ABC):
     @abstractclassmethod
-    def getComments() -> List[Comment]:
+    def getComments(props: GetComments) -> List[Comment]:
         pass
 
     @abstractclassmethod
-    def addComment() -> Comment:
+    def createComment(props: CreateComment) -> Comment:
+        pass
+
+    @abstractclassmethod
+    def getCommentById(id: CommentId) -> Union[Comment, None]:
+        pass
+
+    @abstractclassmethod
+    def updateCommentById(props: UpdateComment) -> Comment:
+        pass
+
+    @abstractclassmethod
+    def deleteCommentById(id: CommentId) -> None:
         pass
